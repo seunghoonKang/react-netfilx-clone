c25b7404cab05cb82648641c1dbb55d2b94015f9
seunghoon <tmdgns4321@gmail.com>
2023-01-21 15:05:18 +0900

    비디오 재생 옵셔널 체이닝

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -67,7 +67,7 @@ const Banner = () => {
           <IFrame
             width="640"
             height="360"
-            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            src={`https://www.youtube.com/embed/${movie.videos.results[0]?.key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0]?.key}`}
             title="YouTube video player"
             frameborder="0"
             allow="autoplay; fullscreen"
