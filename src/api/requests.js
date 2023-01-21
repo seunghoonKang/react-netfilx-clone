ce2b727da2f982d719cb878f81099a46a9dae857
seunghoon <tmdgns4321@gmail.com>
2023-01-21 16:23:40 +0900

    row component 구현

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,7 +1,7 @@
 const requests = {
   fetchNowPlaying: "movie/now_playing",
   fetchNetflixOriginals: "/discover/tv?with_networks=213",
-  fetchTrending: "/trendig/all/week",
+  fetchTrending: "/trending/all/week",
   fetchTopRated: "/movie/top_rated",
   fetchActionMovies: "/discover/movie?with_genres=28",
   fetchComedyMovies: "/discover/movie?with_genres=35",
