5758db3148f7560577872c27fbb743c38e976695
seunghoon <tmdgns4321@gmail.com>
2023-01-22 20:19:24 +0900

    env설정

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "b18e798ff377ef49f1c335283e7c43d6",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });
